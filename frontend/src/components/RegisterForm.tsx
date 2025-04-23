f9b2412e6adccbf34e002970d79cdcbe31f328fe
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-04-24 04:37:47 +0800

    change:modified css

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, Card, message } from "antd";
+import { Form, Input, Button, message } from "antd";
 import { loginUser, registerUser } from "../api/UserAPI";
 import { RegisterUserRequest, UserLoginRequest, UserLoginResponse, } from "../types/User";
 import { useAuth } from "../context/AuthContext";
@@ -48,46 +48,44 @@ const RegistrationForm: React.FC<Props> = ({ setRegistered }) => {
 
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center min-h-screen bg-gray-100" style={{ width: '300px', margin: '0 auto', alignItems: "center" }}>
             {contextHolder}
-            <Card className="rounded-2xl shadow-lg" style={{ maxWidth: "300px", width: "100%", margin: "0 auto" }}>
-                <Form
-                    form={form}
-                    layout="vertical"
-                    onFinish={onFinish}
-                    autoComplete="off"
+            <Form
+                form={form}
+                layout="vertical"
+                onFinish={onFinish}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[{ required: true, message: "Please input your name!" }]}
                 >
-                    <Form.Item
-                        label="Name"
-                        name="name"
-                        rules={[{ required: true, message: "Please input your name!" }]}
-                    >
-                        <Input placeholder="Enter your name" />
-                    </Form.Item>
+                    <Input placeholder="Enter your name" />
+                </Form.Item>
 
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: "Please input your username!" }]}
-                    >
-                        <Input placeholder="Choose a username" />
-                    </Form.Item>
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: "Please input your username!" }]}
+                >
+                    <Input placeholder="Choose a username" />
+                </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: "Please input your password!" }]}
-                    >
-                        <Input.Password placeholder="Enter a password" />
-                    </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: "Please input your password!" }]}
+                >
+                    <Input.Password placeholder="Enter a password" />
+                </Form.Item>
 
-                    <Form.Item style={{ textAlign: "center" }}>
-                        <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-                            Register
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
+                <Form.Item style={{ textAlign: "center" }}>
+                    <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     );
 };
