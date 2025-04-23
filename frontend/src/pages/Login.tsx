e2ee63df17d2c1ca75e5ca8d138a74a6f884fe66
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-04-24 03:40:29 +0800

    Remove unused import

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import LoginForm from '../components/LoginForm'
-import "../css/Login.css"
 import { useNavigate } from 'react-router';
 import { useAuth } from "../context/AuthContext";
 
