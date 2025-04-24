1fb7e84e22c2a4df4b71f09320548659fc132873
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-04-24 08:03:02 +0800

    Added links

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -28,13 +28,27 @@ const Home: React.FC = () => {
   return (
     <>
       {!isAuthenticated ?
-        (<div style={{ maxWidth: "700px" }}>
-          <h2>Writeup</h2>
-          <p>Hi, I'm Chong Yao. I'm a year 2 at NTU reading Computer Science. I enjoy playing competitive FPS, Gacha games, and building keyboards (whenever time and money permits!). I like to think of myself as a sort of explorer, as I've explored different hobbies, games, or activities I think I might enjoy over the years, including 3D printing and soldering for fun. Recently, I've found that I enjoy web development, picking up Golang as my primary backend language, and recently started with React with Typescript for the frontend, with PostgreSQL as my database of choice and a touch of Redis as a cache-aside data store for faster retrievals. I've also touched a little bit on Python and Java for my school modules, having created a Flask backend with SQLite for a group project, and a CLI Java app while trying to adhere to SOLID principles as much as I can, implementing patterns such as repository, publisher-subscriber, and data transfer objects.
+        (
+          <>
+            <div style={{ maxWidth: "700px" }}>
+              <h2>Writeup</h2>
+              <p>Hi, I'm Chong Yao. I'm a year 2 at NTU reading Computer Science. I enjoy playing competitive FPS, Gacha games, and building keyboards (whenever time and money permits!). I like to think of myself as a sort of explorer, as I've explored different hobbies, games, or activities I think I might enjoy over the years, including 3D printing and soldering for fun. Recently, I've found that I enjoy web development, picking up Golang as my primary backend language, and recently started with React with Typescript for the frontend, with PostgreSQL as my database of choice and a touch of Redis as a cache-aside data store for faster retrievals. I've also touched a little bit on Python and Java for my school modules, having created a Flask backend with SQLite for a group project, and a CLI Java app while trying to adhere to SOLID principles as much as I can, implementing patterns such as repository, publisher-subscriber, and data transfer objects.
+                <br></br><br></br>
+                Although my pool of projects might be small right now, I'm always looking to expand my knowledge and skills, even if it's a little bit everyday, with a growing list of things I want to work on and learn, such as gRPC, writing test code, and more about software design, I aim to become a well-rounded engineer in the future.
+              </p>
+            </div>
+            <div>
+              <h2>Links</h2>
+              <a href="https://github.com/mahopon" target="_blank" rel="noopener noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="32" height="32" />
+              </a>&nbsp;&nbsp;
+              <a href="https://www.linkedin.com/in/chong-yao-tan-22bb021a6/" target="_blank" rel="noopener noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" width="32" height="32" />
+              </a>
 
-            Although my pool of projects might be small right now, I'm always looking to expand my knowledge and skills, even if it's a little bit everyday, with a growing list of things I want to work on and learn, such as gRPC, writing test code, and more about software design, I aim to become a well-rounded engineer in the future.
-          </p>
-        </div>)
+            </div>
+          </>
+        )
         :
         (
           <>
